7be77ea7c17186c4270522ec1fad753319afaaba
404pnf <404pnf@exmaple.org>
2014-06-06 11:14:53 +0800

    minitest

--- test/test_nanotest.rb
+++ test/test_nanotest.rb
@@ -22,7 +22,7 @@ class Foo
   include Nanotest
 end
 
-class TestNanotest < MiniTest::TestCase
+class TestNanotest < MiniTest::Test
   def self.test(name, &block)
     define_method("test_#{name.gsub(/\s/, '_')}", &block)
   end
